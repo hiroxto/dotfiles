c11eef8ec89ad7d2cd3d60f9bbf3077380a680d7
Hiroto-K <hiro.yo.yo1610@gmail.com>
2015-10-18 18:17:43 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,17 +4,20 @@ echo -n "ファイルを移動しますか?[y/n]  ->  "
 read res
 case $res in
   [yY])
-    mv .editorconfig ~/.editorconfig
-    mv .zshrc ~/.zshrc
-    mv .nanorc ~/.nanorc
-    mv .vimrc ~/.vimrc
-    mv .tmux.conf ~/.tmux.conf
-    mv .gitignore_global ~/.gitignore_global
-    mv .gitconfig ~/.gitconfig
-    mv .hgignore_global ~/.hgignore_global
-    mv .hgrc ~/.hgrc
-  ;;
-  [nN])
+    files=(
+      .zshrc
+      .vimrc
+      .tmux.conf
+      .editorconfig
+      .nanorc
+      .gitconfig
+      .gitignore_global
+      .hgrc
+      .hgignore_global
+    )
+    for file in ${files[@]}; do
+      mv $file ~/$file
+    done
   ;;
 esac
 
@@ -24,8 +27,6 @@ case $res in
   [yY])
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   ;;
-  [nN])
-  ;;
 esac
 
 echo -n "rbenvをインストールしますか?[y/n]  ->  "
@@ -34,8 +35,6 @@ case $res in
   [yY])
     git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
     git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  ;;
-  [nN])
   ;;
 esac
 
@@ -48,8 +47,6 @@ case $res in
     git clone https://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
     rm -rf /tmp/phpenv
   ;;
-  [nN])
-  ;;
 esac
 
 echo -n "pyenvをインストールしますか?[y/n]  ->  "
@@ -57,8 +54,6 @@ read res
 case $res in
   [yY])
     git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-  ;;
-  [nN])
   ;;
 esac
 
@@ -68,8 +63,6 @@ case $res in
   [yY])
     git clone git://github.com/scopatz/nanorc ~/.nano
     cat nanorc.nanorc >> ~/.nanorc
-  ;;
-  [nN])
   ;;
 esac
 
@@ -81,7 +74,5 @@ case $res in
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     mkdir -p ~/.vim/colors
     curl -sS https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >> ~/.vim/colors/molokai.vim
-  ;;
-  [nN])
   ;;
 esac
