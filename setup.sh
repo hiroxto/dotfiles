63502ac6497a08b5e04a34ab4cab8923c731c909
Hiroto-K <hiro.yo.yo1610@gmail.com>
2015-10-18 18:26:49 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,78 +1,64 @@
 #!/bin/sh
 
 echo -n "ファイルを移動しますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    files=(
-      .zshrc
-      .vimrc
-      .tmux.conf
-      .editorconfig
-      .nanorc
-      .gitconfig
-      .gitignore_global
-      .hgrc
-      .hgignore_global
-    )
-    for file in ${files[@]}; do
-      mv $file ~/$file
-    done
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  files=(
+    .zshrc
+    .vimrc
+    .tmux.conf
+    .editorconfig
+    .nanorc
+    .gitconfig
+    .gitignore_global
+    .hgrc
+    .hgignore_global
+  )
+  for file in ${files[@]}; do
+    mv $file ~/$file
+  done
+fi
 
 echo -n "oh-my-zshをインストールしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 
 echo -n "rbenvをインストールしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
 echo -n "phpenvをインストールしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    git clone https://github.com/CHH/phpenv.git /tmp/phpenv
-    /tmp/phpenv/bin/phpenv-install.sh
-    git clone https://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
-    rm -rf /tmp/phpenv
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  git clone https://github.com/CHH/phpenv.git /tmp/phpenv
+  /tmp/phpenv/bin/phpenv-install.sh
+  git clone https://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
+  rm -rf /tmp/phpenv
+fi
 
 echo -n "pyenvをインストールしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+fi
 
 echo -n "nanorcをセットアップしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    git clone git://github.com/scopatz/nanorc ~/.nano
-    cat nanorc.nanorc >> ~/.nanorc
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  git clone git://github.com/scopatz/nanorc ~/.nano
+  cat nanorc.nanorc >> ~/.nanorc
+fi
 
 echo -n "vimをセットアップしますか?[y/n]  ->  "
-read res
-case $res in
-  [yY])
-    mkdir -p ~/.vim/bundle
-    git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-    mkdir -p ~/.vim/colors
-    curl -sS https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >> ~/.vim/colors/molokai.vim
-  ;;
-esac
+read input
+if [ $input = "y" -o $input = "Y" ]; then
+  mkdir -p ~/.vim/bundle
+  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  mkdir -p ~/.vim/colors
+  curl -sS https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >> ~/.vim/colors/molokai.vim
+fi
