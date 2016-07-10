5de67a99a1971726aaf2d3278a1e5dfa5bf9c4ea
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-10 13:50:31 +0900

    Fix

--- .zsh/init/oh-my-zsh.zsh
+++ .zsh/init/oh-my-zsh.zsh
@@ -7,11 +7,6 @@ export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="robbyrussell"
 
 # oh-my-zsh plugins
-plugins=(archlinux bower bundler coffee composer gem git golang heroku laravel5 node npm nvm pip rails rake rbenv systemd ubuntu vagrant)
-
-# Add brew plugin on OS X
-if [ "$OSTYPE" = darwin* ];then
-  plugins+=(brew)
-fi
+plugins=(archlinux bower bundler brew coffee composer gem git golang heroku laravel5 node npm nvm pip rails rake rbenv systemd ubuntu vagrant)
 
 source $ZSH/oh-my-zsh.sh
