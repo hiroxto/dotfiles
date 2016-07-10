51ba468b3dd81e8372b4b55c350361accdbf5adb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2016-07-10 13:13:58 +0900

    Add brew plugin on OS X

--- .zsh/init/oh-my-zsh.zsh
+++ .zsh/init/oh-my-zsh.zsh
@@ -9,4 +9,9 @@ ZSH_THEME="robbyrussell"
 # oh-my-zsh plugins
 plugins=(archlinux bower bundler coffee composer gem git golang heroku laravel5 node npm nvm pip rails rake rbenv systemd ubuntu vagrant)
 
+# Add brew plugin on OS X
+if [ "$OSTYPE" = darwin* ];then
+  plugins+=(brew)
+fi
+
 source $ZSH/oh-my-zsh.sh
