e28bd8b1b511fc6e97afe5f5084ecc6b93c77ab6
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-05-09 20:23:56 +0900

    Clean up alias

--- .zsh/init/alias.zsh
+++ .zsh/init/alias.zsh
@@ -1,25 +1,4 @@
 # aliasの設定
 
-# 基本的なalias
-alias nano="nano -w"
-alias vi="vim"
-alias rmdir="rm -rf"
 alias mkdir="mkdir -p"
-alias cp="cp -i"
-alias mv="mv -i"
-
-# gitのalias
-alias gdf="git difff"
-alias gds="git diffs"
-
-# grcが使える環境でのみgrcで色をつける設定
-if [ type grc > /dev/null 2>&1 ];then
-  alias mount="grc mount"
-  alias ifconfig="grc ifconfig"
-  alias dig="grc dig"
-  alias -g netstat="grc netstat"
-  alias ping="grc ping"
-  alias ps="grc ps"
-  alias traceroute="grc traceroute"
-  alias gcc="grc gcc"
-fi
+alias rmdir="rm -rf"
