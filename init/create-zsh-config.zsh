c24debe2fb7aabf1eb7544b048cc379285369a14
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-05 19:51:04 +0900

    Fix create-zsh-config.zsh script
    Check symlinks every times

--- init/create-zsh-config.zsh
+++ init/create-zsh-config.zsh
@@ -11,10 +11,20 @@ if [ -d ${zprezto_dir} ]; then
     echo "Skip install."
 else
     git clone --depth 1 --recursive https://github.com/sorin-ionescu/prezto ${zprezto_dir}
-
-    echo "Link zprezto config files"
-    setopt EXTENDED_GLOB
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-    done
 fi
+
+echo "Link zprezto config files"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    link_to="${ZDOTDIR:-$HOME}/.${rcfile:t}"
+
+    echo "Link ${rcfile} to ${link_to}"
+
+    if [ -e ${link_to} ]; then
+        echo "File '${link_to}' is already exist."
+        echo "Skip create symbolic link."
+    else
+        echo "Create symbolic link to ${link_to}"
+        ln -s "$rcfile" "${link_to}"
+    fi
+done
