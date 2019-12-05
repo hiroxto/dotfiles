763fd8bb42a2d979889251b99fdf8438b309d94f
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-05 20:12:39 +0900

    Add anyenv initialize proccess

--- init/setup-anyenv.zsh
+++ init/setup-anyenv.zsh
@@ -9,6 +9,7 @@ anyenv_bin="${anyenv_dir}/bin/anyenv"
 anyenv_plugins_dir="${anyenv_dir}/plugins"
 anyenv_update_dir="${anyenv_dir}/plugins/anyenv-update"
 export ANYENV_ROOT="${anyenv_dir}"
+export SHELL="zsh"
 
 if [ -d ${anyenv_dir} ]; then
     echo "Directory ${anyenv_dir} is already exist."
@@ -27,6 +28,10 @@ if [ -e ${anyenv_update_dir} ];then
 else
     git clone --depth 1 https://github.com/znz/anyenv-update ${anyenv_update_dir}
 fi
+
+echo "Initialize anyenv"
+eval "$(${anyenv_bin} init zsh --no-rehash)"
+eval "$(${anyenv_bin} install --init)"
 
 echo "Install envs"
 for envname in rbenv phpenv pyenv ndenv; do
