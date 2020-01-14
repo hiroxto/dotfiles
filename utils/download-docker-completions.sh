06755c7058ebdbd0051778dd86e11fcd03ab599d
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-15 00:17:02 +0900

    Fix directory name

--- utils/download-docker-completions.sh
+++ utils/download-docker-completions.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-mkdir -p ~/.zsh/local/completion
+mkdir -p ~/.zsh/local/completions
 
 echo "Download docker completions"
-curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/local/completion/_docker
+curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/local/completions/_docker
 
 echo "Download docker-compose completions"
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/local/completion/_docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/local/completions/_docker-compose
