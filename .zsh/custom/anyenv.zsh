be1054512948677150758cddc1ac2c2137ae4cdc
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-04-02 00:08:53 +0900

    anyenv.zsh にコメントを追加

--- .zsh/custom/anyenv.zsh
+++ .zsh/custom/anyenv.zsh
@@ -1,9 +1,12 @@
-# anyenvの設定
+# anyenvの設定を行うファイル
+#
+# RubyGems のパスの設定など, anyenv が絡む物はこのファイルで行う.
 
 # anyenvの初期化
 eval "$(anyenv init - zsh)"
 
-# Gem homeの設定
+# RubyGems の設定
+# 環境変数 GEM_HOME の設定と, Gem の実行ファイルのパスの設定をする
 if type "ruby" > /dev/null 2>&1; then
     dir=`ruby -e 'print Gem.user_dir'`
     export GEM_HOME="${dir}"
