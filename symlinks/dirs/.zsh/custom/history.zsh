6910d40108955bf77d3dff7889cb1064541dc156
hiroxto <hiroxto@gmail.com>
2022-08-06 13:00:04 +0900

    ghqとpecoのキーバインドを作成

--- symlinks/dirs/.zsh/custom/history.zsh
+++ symlinks/dirs/.zsh/custom/history.zsh
@@ -9,3 +9,17 @@ zle -N history-beginning-search-forward-end history-search-end
 # Ctrl + P と Ctrl + N で先頭一致
 bindkey "^p" history-beginning-search-backward-end
 bindkey "^n" history-beginning-search-forward-end
+
+# Ctrl + ] でghq管理下にあるリポジトリを一覧表示
+if type "peco" > /dev/null 2>&1; then
+    function peco-src () {
+        local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+            if [ -n "$selected_dir" ]; then
+                BUFFER="cd ${selected_dir}"
+                zle accept-line
+            fi
+        zle clear-screen
+    }
+    zle -N peco-src
+    bindkey '^]' peco-src
+fi
