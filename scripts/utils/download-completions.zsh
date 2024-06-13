8694580c804f2efb3bcd94ef9b8473f55b337b8c
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-14 00:28:27 +0900

    feat: docker-composeは古いので削除

--- scripts/utils/download-completions.zsh
+++ scripts/utils/download-completions.zsh
@@ -11,12 +11,6 @@ if type "docker" > /dev/null 2>&1; then
     curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/local/completions/_docker
 fi
 
-# docker-composeの補完をダウンロード
-if type "docker-compose" > /dev/null 2>&1; then
-    echo "Download docker-compose completion"
-    curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/local/completions/_docker-compose
-fi
-
 # Kubernetes
 if type "kubectl" > /dev/null 2>&1; then
     echo "Setup kubectl completion"
