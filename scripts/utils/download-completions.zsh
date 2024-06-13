ffc52adc0321a48260343a07dcf091334c1e1ba8
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-14 00:33:04 +0900

    feat: Taskfileの補完を追加

--- scripts/utils/download-completions.zsh
+++ scripts/utils/download-completions.zsh
@@ -23,3 +23,9 @@ if type "minikube" > /dev/null 2>&1; then
     echo "Setup minikube completion"
     minikube completion zsh >| ${COMPLETIONS_DIR}/_minikube
 fi
+
+# task
+if type "minikube" > /dev/null 2>&1; then
+    echo "Setup task completion"
+    curl -sSL https://raw.githubusercontent.com/go-task/task/main/completion/zsh/_task >| ${COMPLETIONS_DIR}/_task
+fi
