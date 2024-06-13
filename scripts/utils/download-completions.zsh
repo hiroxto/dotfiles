a220c5233278bf0c9eab31902cb82fb7e67816b6
Hiroto Kitazawa <hiroxto@gmail.com>
2024-06-14 00:30:34 +0900

    feat: ダウンロード先を変数化

--- scripts/utils/download-completions.zsh
+++ scripts/utils/download-completions.zsh
@@ -3,22 +3,23 @@
 
 set -eu
 
-mkdir -p ~/.zsh/local/completions
+COMPLETIONS_DIR=~/.zsh/local/completions
+mkdir -p $COMPLETIONS_DIR
 
 # dockerの補完をダウンロード
 if type "docker" > /dev/null 2>&1; then
     echo "Download docker completion"
-    curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/local/completions/_docker
+    curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ${COMPLETIONS_DIR}/_docker
 fi
 
 # Kubernetes
 if type "kubectl" > /dev/null 2>&1; then
     echo "Setup kubectl completion"
-    kubectl completion zsh >| ~/.zsh/local/completions/_kubectl
+    kubectl completion zsh >| ${COMPLETIONS_DIR}/_kubectl
 fi
 
 # minikube
 if type "minikube" > /dev/null 2>&1; then
     echo "Setup minikube completion"
-    minikube completion zsh >| ~/.zsh/local/completions/_minikube
+    minikube completion zsh >| ${COMPLETIONS_DIR}/_minikube
 fi
