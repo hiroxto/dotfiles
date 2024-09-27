f300cb9169441e100ba66dfdfccb8f3c5a6b5d2c
Hiroto Kitazawa <hiroxto@gmail.com>
2024-09-27 23:28:30 +0900

    chore: Travis CIは使わないので削除

--- symlinks/dirs/.zsh/custom/local.zsh
+++ symlinks/dirs/.zsh/custom/local.zsh
@@ -2,11 +2,6 @@
 #
 # ~/.zshrc.local などのローカルだけで設定される物はここで設定する
 
-# Travi CI の gem で入る補完ファイルがあったら読み込む
-if [ -f ~/.travis/travis.sh ];then
-  source ~/.travis/travis.sh
-fi
-
 # Local のみの zsh の設定があったら読み込む
 if [ -f ~/.zshrc.local ];then
   source ~/.zshrc.local
