e8d42cca726e94f067dc17ffb83d017edc6f2ef7
Hiroto Kitazawa <hiroxto@gmail.com>
2025-11-21 01:30:34 +0900

    feat: 補完のダウンロードを削除してコマンドで補完を設定する

--- symlinks/dirs/.zsh/custom/completion.zsh
+++ symlinks/dirs/.zsh/custom/completion.zsh
@@ -1,13 +1,33 @@
 # コマンドの補完の設定を行うファイル
 #
-# コマンドの引数でシェルの補完を作成出来る場合は, このファイルで行う.
+# コマンドの引数でシェルの補完を作成出来る場合はこのファイルで行う
 
-# gh コマンドの補完設定
+# gh
 if type "gh" > /dev/null 2>&1; then
     eval "$(gh completion -s zsh)"
 fi
 
-# brew コマンドの補完設定
+# brew
 if type "brew" > /dev/null 2>&1; then
     FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
+fi
+
+# docker
+if type "docker" > /dev/null 2>&1; then
+    eval "$(docker completion zsh)"
+fi
+
+# Kubernetes
+if type "docker" > /dev/null 2>&1; then
+    eval "$(kubectl completion zsh)"
+fi
+
+# minikube
+if type "minikube" > /dev/null 2>&1; then
+    eval "$(minikube completion zsh)"
+fi
+
+# task
+if type "task" > /dev/null 2>&1; then
+    eval "$(task --completion zsh)"
 fi
