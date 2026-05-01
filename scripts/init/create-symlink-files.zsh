e0f9c5ad7dd279b2bdf942c4a9fbf28ffc3639c5
Hiroto Kitazawa <hiroxto@gmail.com>
2026-05-01 20:50:06 +0900

    feat: Ghosttyの設定をdotfilesで同期する

--- scripts/init/create-symlink-files.zsh
+++ scripts/init/create-symlink-files.zsh
@@ -18,3 +18,27 @@ for file in `ls -A1 ${HOME}/dotfiles/symlinks/files/auto-link/`; do
         ln -s ${link_from} ${link_to}
     fi
 done
+
+# dotfiles の symlinks/files/manual-link 以下に入っているファイルを指定したパスへリンク
+manual_link_files=(
+    ghostty/config:.config/ghostty/config
+)
+
+for file in ${manual_link_files[@]}; do
+    link_from="${HOME}/dotfiles/symlinks/files/manual-link/${file%%:*}"
+    link_to="${HOME}/${file#*:}"
+    link_to_dir=`dirname ${link_to}`
+
+    echo "Create directory : ${link_to_dir}"
+    mkdir -p ${link_to_dir}
+
+    echo "Link ${link_from} to ${link_to}"
+
+    if [ -e ${link_to} ]; then
+        echo "File '${link_to}' is already exist."
+        echo "Skip create symbolic link."
+    else
+        echo "Create symbolic link to ${link_to}"
+        ln -s ${link_from} ${link_to}
+    fi
+done
